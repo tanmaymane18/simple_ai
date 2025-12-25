68b50792f83d8381a415bab0b050a2d937f3dcad
Tanmay Mane <tanmaymane8@gmail.com>
2025-12-25 19:39:15 +0000

    a bare minimum code assistant

---
+++ main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"os"
+
+	"google.golang.org/adk/agent"
+	"google.golang.org/adk/agent/llmagent"
+	"google.golang.org/adk/agent/workflowagents/sequentialagent"
+	"google.golang.org/adk/cmd/launcher"
+	"google.golang.org/adk/cmd/launcher/full"
+	"google.golang.org/adk/model/gemini"
+	"google.golang.org/adk/tool"
+	"google.golang.org/adk/tool/functiontool"
+	"google.golang.org/genai"
+)
+
+type WriteInput struct {
+	FileName string `json:"file_name"`
+	Content  string `json:"content"`
+}
+
+type ReadInput struct {
+	FileName string `json:"file_name"`
+}
+
+func main() {
+	ctx := context.Background()
+
+	model, err := gemini.NewModel(ctx, "gemini-2.5-flash", &genai.ClientConfig{APIKey: os.Getenv("GOOGLE_API_KEY")})
+	if err != nil {
+		log.Fatalf("Failed to create model: %v", err)
+	}
+
+	write_file_tool, err := functiontool.New(functiontool.Config{
+		Name:        "write_file",
+		Description: "Writes content to the requested file.",
+	},
+		func(ctx tool.Context, input WriteInput) (string, error) {
+			file, err := os.OpenFile(input.FileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				return "Failed to open/create file " + input.FileName, err
+			}
+			defer file.Close()
+			if _, err := file.WriteString(input.Content); err != nil {
+				return "Failed to write the content to file " + input.FileName, err
+			}
+			return "Successfully written the content to file " + input.Content, nil
+		},
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to create write_file tool %v", err)
+	}
+
+	read_file_tool, err := functiontool.New(functiontool.Config{
+		Name:        "read_file",
+		Description: "Reads content of the requested file.",
+	},
+		func(ctx tool.Context, input ReadInput) (string, error) {
+			var content_bytes []byte
+			if content_bytes, err = os.ReadFile(input.FileName); err != nil {
+				return "Failed to read the content of the file " + input.FileName, err
+			}
+			return string(content_bytes), nil
+		},
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to create read_file tool %v", err)
+	}
+
+	analyzerAgent, err := llmagent.New(llmagent.Config{
+		Name:        "analyzer_agent",
+		Model:       model,
+		Description: "Analyzes user request and the code base",
+		Instruction: `You are excellent at understanding the user's request. Understand what user wants, analyze the code base by reading only the necessary files and genearting analytical thoughts about each file. 
+			
+			Use 'read_file' tool to read the contents of the file.
+			Always read the content of the files before analyzing and generating thoughts about it.
+
+		Example output:
+		## Analysis
+		### User request: // whatever the user request is
+
+		### file_name_1
+		// your analysis of the file_name_1
+
+
+		### file_name_2
+		// your analysis of the file_name_2
+		.
+		.
+		.
+
+		### Final Thoughts
+		// Your final thoughts
+		.
+		.
+		.
+
+		## End of analysis
+		`,
+		OutputKey: "analysis",
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create analyzer_agent %v", err)
+	}
+
+	plannerAgent, err := llmagent.New(llmagent.Config{
+		Name:        "planner_agent",
+		Model:       model,
+		Description: "Generates execution plan",
+		Instruction: `You are an excellent at planning software engineering tasks.
+		Given is the analysis of what user wants and the repo analysis.
+
+		### User Request and Repo Analysis
+		{analysis}
+		
+		Generate a implementation plan in markdown bullet-points to resolve the user request.
+		`,
+		OutputKey: "implementation_plan",
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create planner_agent %v", err)
+	}
+
+	codeAgent, err := llmagent.New(llmagent.Config{
+		Name:        "code_agent",
+		Model:       model,
+		Description: "Writes code",
+		Instruction: `You are an excellent programmer who helps user by writing code. 
+
+			Use 'write_file' tool to write contents to the file. 
+			Always write code to a file with the code without any extra lines, prefix or suffix.
+			
+			Use 'read_file' tool to read the contents of the file.
+			Always read the content of existing file before writing to the the file.
+
+
+		Given is the user request, repo analysis and implementation plan, execute things accordingly.
+
+		### Analysis
+
+		{analysis}
+
+		### Implementation Plan
+		{implementation plan}
+		`,
+		Tools: []tool.Tool{
+			write_file_tool,
+			read_file_tool,
+		},
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create code_agent %v", err)
+	}
+
+	codepipeline, err := sequentialagent.New(sequentialagent.Config{
+		AgentConfig: agent.Config{
+			Name: "CodePipelineAgent",
+			SubAgents: []agent.Agent{
+				analyzerAgent,
+				plannerAgent,
+				codeAgent,
+			},
+			Description: "Executes a software engineering task request.",
+		},
+	})
+
+	rootagent := codepipeline
+
+	config := &launcher.Config{
+		AgentLoader: agent.NewSingleLoader(rootagent),
+	}
+
+	l := full.NewLauncher()
+	if err := l.Execute(ctx, config, os.Args[1:]); err != nil {
+		log.Fatalf("Failed to execute agent: %v\n\n%s", err, l.CommandLineSyntax())
+	}
+}
