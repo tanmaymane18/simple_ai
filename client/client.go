873a66f7ab9c02442791869a16a20ea425d9710e
Tanmay Mane <tanmaymane8@gmail.com>
2025-12-28 18:23:53 +0000

    refactor client

--- client/client.go
+++ client/client.go
@@ -11,55 +11,6 @@ import (
 	"google.golang.org/genai"
 )
 
-type EventActions struct {
-	StateDelta    map[string]any   `json:"stateDelta"`
-	ArtifactDelta map[string]int64 `json:"artifactDelta"`
-}
-
-type Event struct {
-	ID                 string                   `json:"id"`
-	Time               int64                    `json:"time"`
-	InvocationID       string                   `json:"invocationId"`
-	Branch             string                   `json:"branch"`
-	Author             string                   `json:"author"`
-	Partial            bool                     `json:"partial"`
-	LongRunningToolIDs []string                 `json:"longRunningToolIds"`
-	Content            *genai.Content           `json:"content"`
-	GroundingMetadata  *genai.GroundingMetadata `json:"groundingMetadata"`
-	TurnComplete       bool                     `json:"turnComplete"`
-	Interrupted        bool                     `json:"interrupted"`
-	ErrorCode          string                   `json:"errorCode"`
-	ErrorMessage       string                   `json:"errorMessage"`
-	Actions            EventActions             `json:"actions"`
-}
-type Session struct {
-	ID        string         `json:"id"`
-	AppName   string         `json:"appName"`
-	UserID    string         `json:"userId"`
-	UpdatedAt int64          `json:"lastUpdateTime"`
-	Events    []Event        `json:"events"`
-	State     map[string]any `json:"state"`
-}
-
-type AdkRequest struct {
-	AppName string `json:"appName"`
-
-	UserId string `json:"userId"`
-
-	SessionId string `json:"sessionId"`
-
-	NewMessage genai.Content `json:"newMessage"`
-
-	Streaming bool `json:"streaming,omitempty"`
-
-	StateDelta *map[string]any `json:"stateDelta,omitempty"`
-}
-
-type AdkClient struct {
-	sv_url      string
-	adk_session Session
-}
-
 func InitAdkClient() AdkClient {
 	return AdkClient{
 		sv_url: "http://localhost:8080/api",
@@ -133,10 +84,4 @@ func (ac *AdkClient) MakeRequest(message string) {
 	} else {
 		fmt.Printf("Reponse: \n\n%v", events[len(events)-1].Content.Parts[0].Text)
 	}
-}
-
-func main() {
-	ac := InitAdkClient()
-	ac.CreateSession()
-	ac.MakeRequest("Add colors in py_lazygit")
 }
